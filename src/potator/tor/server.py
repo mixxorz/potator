9e8701646e565f826665a2092c4a547dd31cc94e
Mitchel Cabuloy <mixxorz@gmail.com>
2014-10-07 12:35:37 +0800

    Initial server code. Forked from Potator

---
+++ src/potator/tor/server.py
@@ -0,0 +1,76 @@
+from collections import deque
+
+from .util import settings
+from twisted.internet.endpoints import TCP4ClientEndpoint
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import NetstringReceiver
+from txsocksx.client import SOCKS5ClientEndpoint
+
+
+class NodeConnection(NetstringReceiver):
+
+    def connectionLost(self, reason):
+        # Remove node from node list if connection is lost
+        pair = next((x for x in self.factory.nodes if x[1] is self), None)
+        if pair:
+            print 'Removing %s' % pair[0]
+            self.factory.nodes.remove(pair)
+
+    def stringReceived(self, string):
+        self.factory.server.receive_buffer.append(string)
+
+
+class NodeFactory(Factory):
+
+    def __init__(self, server):
+        self.nodes = []
+        self.server = server
+
+    def buildProtocol(self, addr):
+        protocol = NodeConnection()
+        protocol.factory = self
+        return protocol
+
+
+class Server(object):
+
+    def __init__(self, reactor):
+        self.endpoint = TCP4ClientEndpoint(
+            reactor, '127.0.0.1', settings.SOCKS_PORT)
+
+        self.receive_buffer = deque()
+        self.send_buffer = deque()
+
+        # Create the factory
+        self.factory = NodeFactory(self)
+
+        # Starts the listening server
+        reactor.listenTCP(settings.SERVER_PORT, self.factory)
+
+    def print_err(self, err):
+        pass
+        # print err
+
+    def connectionFailure(self, err):
+        return err
+
+    def sendSpore(self, protocol, spore_string):
+        protocol.sendString(spore_string)
+        return protocol
+
+    def registerNode(self, protocol, factory, onion_url):
+        print 'Register %s' % onion_url
+        factory.nodes.append((onion_url, protocol,))
+        return protocol
+
+    def connectTorSocks(self, host, factory):
+        # Host must be str, not unicode
+        if type(host) == unicode:
+            host = host.encode('utf-8')
+
+        endpoint = SOCKS5ClientEndpoint(
+            host, settings.HIDDEN_SERVICE_PORT, self.endpoint)
+
+        d = endpoint.connect(factory)
+        d.addErrback(self.connectionFailure)
+        return d
