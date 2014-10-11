a93d3ced05c3276d4758822c9f2b512910e49f6a
Mitchel Cabuloy <mixxorz@gmail.com>
2014-10-11 21:02:50 +0800

    Added code to measure transfer speed

--- src/potator/stats.py
+++ src/potator/stats.py
@@ -11,11 +11,26 @@ class StatPrinter(threading.Thread):
         self.running = True
 
     def run(self):
+        prev_sent = 0
+        prev_recv = 0
         while self.running:
             # print 'TOR: S: %sb R: %sb'
+            cur_sent = self.tun.sent_bytes
+            cur_recv = self.tun.received_bytes
+
+            speed_sent = (cur_sent - prev_sent)
+            speed_recv = (cur_recv - prev_recv)
+
+            print '\r                                                         ',
+            print '\rCurrent speed: S: %dkb/s R: %dkb/s' % (speed_sent / 1024,
+                                                            speed_recv / 1024),
+
+            prev_sent = cur_sent
+            prev_recv = cur_recv
+
+            # print 'TUN: S: %db R: %db\r' % (self.tun.sent_bytes,
+            #                                 self.tun.received_bytes),
             time.sleep(1)
-            print 'TUN: S: %db R: %db\r' % (self.tun.sent_bytes,
-                                            self.tun.received_bytes),
 
     def stop(self):
         self.running = False
